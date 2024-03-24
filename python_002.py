0bb3a574ff71671703f4d3054c99f6b29b2e8f63
MateusMonMendes <149646819+MateusMonMendes@users.noreply.github.com>
2024-03-24 10:25:06 -0300

    Delete python_002.py

--- python_002.py
+++
@@ -1,6 +0,0 @@
-# Faça um programa que peça um número e então mostre a mensagem. "O número informado foi [número]."
-def pedir_numero (numero):
-  numero = float(input('Digite o numero: ')
-  return f'O número informado é {numero}'
-
-print(pedir_numero)
